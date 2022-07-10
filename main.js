0087e0b9820552c974ddd1b569da97920409bc40
LoonyYesh <pyeswanth131@gmail.com>
2022-07-10 11:54:10 +0530

    updated

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 function filter(){
     var exp = parseInt(document.getElementById("exp").value);
-    var markedCheckbox = document.querySelectorAll('input[type="checkbox"]:checked');  
+    var selectedSkills = document.querySelectorAll('input[name="skills"][type="checkbox"]:checked');  
+    var selectedCatagory = document.querySelectorAll('select[name="catagory"]')[0].options;  
     var trs = document.querySelectorAll('tr[class="vals"]');
     for (var tr of trs){
         tr.style.visibility='hidden';
     }
-    for (var checkbox of markedCheckbox) {  
-        for(let tr of trs){
-            let temp_exp = parseInt(tr.id.split(",")[2]);
-            console.log(isNaN(exp))
-            if(temp_exp<=exp || isNaN(exp)){
-                if(tr.id.includes(checkbox.value)){
+    selectedCatagory = selectedCatagory[selectedCatagory.selectedIndex].text;
+    for (var skill of selectedSkills) {  
+        for(let tr of trs){            
+            let temp_array = tr.id.split(",");
+            let temp_sno = temp_array[0];
+            let temp_catagory = temp_array[1];
+            let temp_minExp = parseInt(temp_array[2]);
+            let temp_maxExp = parseInt(temp_array[3]);
+            let temp_exp = parseInt(tr.id.split(",")[1]);
+            console.log(exp>=temp_exp)
+            if((selectedCatagory == temp_catagory || selectedCatagory == "others") && (exp>=temp_exp || isNaN(exp))){
+                if(tr.id.includes(skill.value)){
                     tr.style.visibility='visible';
-                    document.body.append(checkbox.value + ' ');  
+                    document.body.append(skill.value + ' ');  
                 }
             }
         }
